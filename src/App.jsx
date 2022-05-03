b7f004d03b5f48203822b7452ebc15f38b79f684
Vijendra <docs.vijendra@gmail.com>
2022-05-03 10:30:05 +0530

    add allroutes module inside app.jsx file

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+// import './App.css'
+// import './seePost.css'
+// import { LoginPage } from './Components/LoginPage/LoginPage'
+// import { LeftMenu } from './Components/LeftMenu/LeftMenu'
+// import { Profile } from './Components/Profile/Profile'
+// import { WhotoFollow } from "./Components/WhotoFollow"
+// import { Post} from './Components/Post/Post'
+// import {FloatingActionButtonZoom} from './Components/FloatingActionButtonZoom'
+// import { Home } from './Components/Home/Home'
+// import { SearchBox } from './Components/SeachBox/SearchBox'
+// import { HomeBody } from './Components/HomeBody/HomeBody'
+// import { ProfileHome } from './Components/ProfileHome/ProfileHome'
+// import { Login } from './Components/LoginPage/Login'
+import { AllRoutes } from './Components/AllRoutes/AllRoutes'
+// import { Login, LoginPage } from './Components/LoginPage/Login'
+// import { SearchBox } from "./Components/SearchBox/SearchBox"
+
+
+
+function App() {
+
+  return (
+    <div className="App">
+      <AllRoutes/>
+      {/* <Login/> */}
+      {/* <Login/> */}
+      {/* <LoginPage/> */}
+      {/* <ProfileHome/> */}
+      {/* <HomeBody/> */}
+      {/* <Home/> */}
+      {/* <SearchBox/> */}
+      {/* <Profile/> */}
+      {/* <FloatingActionButtonZoom/> */}
+      {/* <WhotoFollow/>
+      <Post/> */}
+      {/* <Post/> */}
+      {/* <SearchBox/> */}
+      {/* <LeftMenu/> */}
+    </div>
+  )
+}
+
+export default App
