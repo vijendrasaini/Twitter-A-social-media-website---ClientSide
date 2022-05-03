b681e2e73b81e39d09533c87dff629e86807a090
Vijendra <docs.vijendra@gmail.com>
2022-05-03 14:32:52 +0530

    all events are removed .

--- src/Components/SearchBar/PortableSearchBox.jsx
+++ src/Components/SearchBar/PortableSearchBox.jsx
@@ -1,16 +1,11 @@
-import { Box } from "@mui/material"
-import { useDispatch, useSelector } from "react-redux"
-import { setIsSearching } from "../../Redux/actionCreators"
+import { useSelector } from "react-redux"
 import { SearchBox } from "./SearchBox"
 import { SearchResultBox } from "./SearchResultBox"
 
 export const PortableSearchBox = () => {
     const { isSearching} = useSelector(store => store)
-    const dispatch = useDispatch()
     return (
-        <div
-            onClick={()=>dispatch(setIsSearching(!isSearching))}
-        >
+        <div>
             <SearchBox />
             {isSearching && <SearchResultBox />}
         </div>
