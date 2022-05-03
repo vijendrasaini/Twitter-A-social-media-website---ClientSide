16d61fbf83c6aa36d3f112a4937f36aedb8b4fc3
Vijendra <docs.vijendra@gmail.com>
2022-05-03 10:33:32 +0530

    setSearchResult actionCreator is added.

---
+++ src/Redux/actionCreators.js
@@ -0,0 +1,13 @@
+import { ISSEARCHING, SETSEARCHRESULT, SETUSER} from './actions'
+
+
+export const setUser = (payload)=>({ type : SETUSER , payload})
+export const setSearchResult = (payload)=>({type : SETSEARCHRESULT, payload})
+export const setIsSearching = (payload)=>({type : ISSEARCHING, payload})
+
+export const performSearch = (searchText)=> async (dispatch)=>{
+    const url = `http://localhost:7000/search?search=${searchText}`
+    const response = await fetch(url)
+    const result = await response.json()
+    dispatch(setSearchResult(result))
+}
