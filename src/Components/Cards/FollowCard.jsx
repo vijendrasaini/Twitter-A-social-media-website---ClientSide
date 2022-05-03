87a9cdef82806eb2841dbbed341dca2d522318de
Vijendra <docs.vijendra@gmail.com>
2022-05-03 10:35:27 +0530

    card to show search result

---
+++ src/Components/Cards/FollowCard.jsx
@@ -0,0 +1,23 @@
+import { Avatar, CardHeader, Typography } from "@mui/material"
+
+
+
+export const FollowCard = ({username, name, avatar}) => {
+
+
+    return (
+        <CardHeader
+            sx={{ '&:hover': { background: '#F2F2F2' }, height: "50px" }}
+            avatar={
+                <Avatar sx={{ width: "56px", height: "56px" }} aria-label="recipe" src={avatar || "https://pbs.twimg.com/profile_images/1503591435324563456/foUrqiEw_400x400.jpg"} />
+            }
+            title={<Typography paragraph p={0} m={0}
+                sx={{ fontWeight: "bold" }}
+            >{name||"Elon Musk"}
+            </Typography>}
+            subheader={<Typography paragraph p={0} m={0} sx={{ color: "#536471" }}
+            >{username ||"@elonmusk"}
+            </Typography>}
+        />
+    )
+}
