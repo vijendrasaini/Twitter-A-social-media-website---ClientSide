d5bd25e6198d05262a5c4dcfa53574b974bf9917
Vijendra <docs.vijendra@gmail.com>
2022-05-03 13:52:37 +0530

    fixed card clicking issue

--- src/Components/Cards/FollowCard.jsx
+++ src/Components/Cards/FollowCard.jsx
@@ -1,23 +1,25 @@
 import { Avatar, CardHeader, Typography } from "@mui/material"
-import { Link} from 'react-router-dom'
+import { Link, useNavigate} from 'react-router-dom'
 
 
 export const FollowCard = ({username, name, avatar}) => {
-    
-    console.log({username})
+    const navigate = useNavigate()
     return (
-        <Link to={`/${username}`} style={{ color : 'black', textDecoration : "none"}}>
+        <Link
+            to={`/${username}`} 
+            style={{ color : 'black', textDecoration : "none"}}
+            >
             <CardHeader
-                sx={{ '&:hover': { background: '#F2F2F2' }, height: "50px" }}
+                sx={{ '&:hover': { background: '#F2F2F2' }, height: "50px" ,cursor : "poiner",}}
                 avatar={
-                    <Avatar sx={{ width: "56px", height: "56px" }} aria-label="recipe" src={avatar || "https://pbs.twimg.com/profile_images/1503591435324563456/foUrqiEw_400x400.jpg"} />
+                    <Avatar sx={{  width: "56px", height: "56px" }} aria-label="recipe" src={avatar} />
                 }
                 title={<Typography paragraph p={0} m={0}
                     sx={{ fontWeight: "bold" }}
-                >{name||"Elon Musk"}
+                >{name}
                 </Typography>}
                 subheader={<Typography paragraph p={0} m={0} sx={{ color: "#536471" }}
-                >{username ||"@elonmusk"}
+                >{username}
                 </Typography>}
             />
         </Link>
