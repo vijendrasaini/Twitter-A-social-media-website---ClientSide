67532324eaa492c3d5d794810c0bbbb40430b9e7
Vijendra <docs.vijendra@gmail.com>
2022-05-03 13:53:28 +0530

    onclick event issue is resolved

--- src/Components/SearchBar/PortableSearchBox.jsx
+++ src/Components/SearchBar/PortableSearchBox.jsx
@@ -1,15 +1,18 @@
 import { Box } from "@mui/material"
-import { useSelector } from "react-redux"
+import { useDispatch, useSelector } from "react-redux"
+import { setIsSearching } from "../../Redux/actionCreators"
 import { SearchBox } from "./SearchBox"
 import { SearchResultBox } from "./SearchResultBox"
 
 export const PortableSearchBox = () => {
     const { isSearching} = useSelector(store => store)
-
+    const dispatch = useDispatch()
     return (
-        <Box>
+        <div
+            onClick={()=>dispatch(setIsSearching(!isSearching))}
+        >
             <SearchBox />
             {isSearching && <SearchResultBox />}
-        </Box>
+        </div>
     )
 }
