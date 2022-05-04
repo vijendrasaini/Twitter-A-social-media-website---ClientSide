8dd19ee3ae7aeafce6a2ef5155ea3d9efb0d3064
Vijendra <docs.vijendra@gmail.com>
2022-05-04 18:31:03 +0530

    console.logs are removed.

--- src/Redux/actionCreators.js
+++ src/Redux/actionCreators.js
@@ -7,6 +7,8 @@ export const setIsSearching = (payload)=>({type : ISSEARCHING, payload})
 export const toggleFollowingStatus = (payload) => ({ type : FOLLOWING_STATUS, payload})
 
 export const performSearch = (searchText)=> async (dispatch)=>{
+    if(searchText == "")
+        return dispatch(setSearchResult([]))
     const url = `http://localhost:7000/search?search=${searchText}`
     const response = await fetch(url)
     const result = await response.json()
