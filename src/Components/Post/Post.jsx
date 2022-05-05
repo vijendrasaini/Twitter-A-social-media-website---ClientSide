af6aabb412318c6bfdcf935a9af46ff73b256b56
Vijendra <docs.vijendra@gmail.com>
2022-05-05 12:00:19 +0530

    fixing bugs

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,4 +1,4 @@
-import { styled } from '@mui/material/styles';
+// import { styled } from '@mui/material/styles';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import CardContent from '@mui/material/CardContent';
@@ -15,6 +15,17 @@ import IosShareIcon from '@mui/icons-material/IosShare';
 import { Box, Grid, Stack } from '@mui/material';
 import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 import VerifiedIcon from '@mui/icons-material/Verified';
+
+import { styled, alpha } from '@mui/material/styles';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import EditIcon from '@mui/icons-material/Edit';
+import Divider from '@mui/material/Divider';
+import ArchiveIcon from '@mui/icons-material/Archive';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+// import Button from '@mui/material/Button';
+// import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
+// import KeyboardArrowDownIcon from '@mui/icons-material/KeyboardArrowDown';
 
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux';
@@ -52,6 +63,49 @@ function timeSince(timeStamp, date) {
         return day + " " + month + year;
     }
 }
+
+const StyledMenu = styled((props) => (
+    <Menu
+      elevation={0}
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right',
+      }}
+      transformOrigin={{
+        vertical: 'top',
+        horizontal: 'right',
+      }}
+      {...props}
+    />
+  ))(({ theme }) => ({
+    '& .MuiPaper-root': {
+      borderRadius: 6,
+      marginTop: theme.spacing(1),
+      minWidth: 180,
+      color:
+        theme.palette.mode === 'light' ? 'rgb(55, 65, 81)' : theme.palette.grey[300],
+      boxShadow:
+        'rgb(255, 255, 255) 0px 0px 0px 0px, rgba(0, 0, 0, 0.05) 0px 0px 0px 1px, rgba(0, 0, 0, 0.1) 0px 10px 15px -3px, rgba(0, 0, 0, 0.05) 0px 4px 6px -2px',
+      '& .MuiMenu-list': {
+        padding: '4px 0',
+      },
+      '& .MuiMenuItem-root': {
+        '& .MuiSvgIcon-root': {
+          fontSize: 18,
+          color: theme.palette.text.secondary,
+          marginRight: theme.spacing(1.5),
+        },
+        '&:active': {
+          backgroundColor: alpha(
+            theme.palette.primary.main,
+            theme.palette.action.selectedOpacity,
+          ),
+        },
+      },
+    },
+  }));
+
+
 export const Post = ({ title, avatar, image, name, username, createdAt, likes, _id }) => {
 
     const [expanded, setExpanded] = useState(false)
@@ -59,15 +113,15 @@ export const Post = ({ title, avatar, image, name, username, createdAt, likes, _
     const { user: { username: self } } = useSelector(store => store)
 
     const [isLiked, setIsLiked] = useState(likes.includes(self) ? true : false)
-    const [countLikes, setCountLikes ] = useState(likes.length)
+    const [countLikes, setCountLikes] = useState(likes.length)
 
-    async function handleLike(){
+    async function handleLike() {
         try {
             const url = `${BASE_URL}/create-post/${self}/${_id}`
             const response = await axios.post(url)
-            if(response.data.status == 'success'){
+            if (response.data.status == 'success') {
                 setIsLiked(!isLiked)
-                setCountLikes(isLiked ? countLikes - 1 : countLikes + 1)
+                setCountLikes(response.data.likes)
             }
         } catch (error) {
             console.log(error.message)
@@ -75,6 +129,14 @@ export const Post = ({ title, avatar, image, name, username, createdAt, likes, _
     }
 
     const handleExpandClick = () => setExpanded(!expanded)
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
     return (
         <Card sx={{
             width: 664,
@@ -187,12 +249,12 @@ export const Post = ({ title, avatar, image, name, username, createdAt, likes, _
                             spacing={0.7}
                         >
 
-                            <span onClick={()=> handleLike()}>
+                            <span onClick={() => handleLike()}>
                                 <IconButton aria-label="share">
                                     {
-                                        isLiked ? <FavoriteIcon sx={{color : "red"}}/> : <FavoriteBorderIcon/>
+                                        isLiked ? <FavoriteIcon sx={{ color: "red" }} /> : <FavoriteBorderIcon />
                                     }
-                                    
+
                                 </IconButton>
                             </span>
                             <Typography paragraph sx={{ padding: "8px 10px 0px 0px" }}>{countLikes}</Typography>
@@ -209,9 +271,38 @@ export const Post = ({ title, avatar, image, name, username, createdAt, likes, _
                     </Stack>
                 </Stack>
                 <Stack>
-                    <IconButton aria-label="settings">
-                        <MoreHorizIcon />
-                    </IconButton>
+                    <span onClick={handleClick}>
+                        <IconButton aria-label="settings">
+                            <MoreHorizIcon />
+                        </IconButton>
+                    </span>
+                    <StyledMenu
+                        id="demo-customized-menu"
+                        MenuListProps={{
+                            'aria-labelledby': 'demo-customized-button',
+                        }}
+                        anchorEl={anchorEl}
+                        open={open}
+                        onClose={handleClose}
+                    >
+                        <MenuItem onClick={handleClose} disableRipple>
+                            <EditIcon />
+                            Edit
+                        </MenuItem>
+                        <MenuItem onClick={handleClose} disableRipple>
+                            <FileCopyIcon />
+                            Duplicate
+                        </MenuItem>
+                        <Divider sx={{ my: 0.5 }} />
+                        <MenuItem onClick={handleClose} disableRipple>
+                            <ArchiveIcon />
+                            Archive
+                        </MenuItem>
+                        <MenuItem onClick={handleClose} disableRipple>
+                            <MoreHorizIcon />
+                            More
+                        </MenuItem>
+                    </StyledMenu>
                 </Stack>
             </Stack>
         </Card >
