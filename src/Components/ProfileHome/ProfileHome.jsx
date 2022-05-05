27802ce7bf156d728d1295471025a7190c099be1
Vijendra <docs.vijendra@gmail.com>
2022-05-05 12:46:14 +0530

    import statement is modified for whotofollow box

--- src/Components/ProfileHome/ProfileHome.jsx
+++ src/Components/ProfileHome/ProfileHome.jsx
@@ -1,48 +1,48 @@
 import { Box, Grid, Stack, Typography, IconButton } from "@mui/material"
 import { LeftMenu } from "../LeftMenu/LeftMenu"
-import { Privacy } from "../Privacy"
 import { WhotoFollow } from "../WhotoFollow"
-import { Profile } from "../Profile/Profile"
 
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import VerifiedIcon from '@mui/icons-material/Verified';
 
 import { useNavigate, useParams } from "react-router-dom"
-import { useDispatch, useSelector} from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { useEffect, useState } from "react"
 import { PortableSearchBox } from "../SearchBar/PortableSearchBox"
 import { BASE_URL } from "../../UniversalData/univeralData"
 import { toggleFollowingStatus } from "../../Redux/actionCreators"
+import { Privacy } from "../Privacy";
+import { Profile } from "../Profile/Profile";
 
 
 export const ProfileHome = () => {
-    
+
     const [profile, setProfile] = useState({})
     // const [fs, setFs] = useState(null)
-    
-    const { username : usernameB } = useParams()
+
+    const { username: usernameB } = useParams()
     const navigate = useNavigate()
     const dispatch = useDispatch()
-    const { user : { username}, followingStatus} = useSelector(store => store)
-    
-    useEffect(()=>{
+    const { user: { username }, followingStatus } = useSelector(store => store)
+
+    useEffect(() => {
         getProfile()
-    },[usernameB])
-    async function getProfile(){
+    }, [usernameB])
+    async function getProfile() {
         const res1 = await fetch(`${BASE_URL}/profile/${username}/${usernameB}`)
         const profile = await res1.json()
         setProfile(profile)
         // console.log(profile)
         // setFs(profile.status)
         dispatch(toggleFollowingStatus({
-            follow : profile.status? true : false,
-            followers : profile.followers
+            follow: profile.status ? true : false,
+            followers: profile.followers
         }))
         // console.log({followingStatus})
     }
-    useEffect(()=>{
-        
-    },[followingStatus])
+    useEffect(() => {
+
+    }, [followingStatus])
     return (
         <>
             <Grid
@@ -51,15 +51,15 @@ export const ProfileHome = () => {
                 <Stack direction={"row"}>
                     <LeftMenu />
                     <Stack>
-                        <Stack direction="row" spacing={4} sx={{ position: "fixed", background: "white", zIndex: 5,height : 53 }}>
+                        <Stack direction="row" spacing={4} sx={{ position: "fixed", background: "white", zIndex: 5, height: 53 }}>
                             <Box>
                                 <Stack
                                     sx={{ boxShadow: "none", maxHeight: 0 }}
                                 >
                                     <Stack direction={"row"}
-                                    sx={{ color: 'black', background: '#EEEEEE', paddingTop: 0.8, width: 664 }}>
+                                        sx={{ color: 'black', background: '#EEEEEE', paddingTop: 0.8, width: 664 }}>
                                         <Box mr={2} sx={{ fontWeight: 'bold', color: "black" }}>
-                                            <IconButton size='large' sx={{ color: "black" }} onClick={()=>navigate('/home')}>
+                                            <IconButton size='large' sx={{ color: "black" }} onClick={() => navigate('/home')}>
                                                 <ArrowBackIcon />
                                             </IconButton>
                                         </Box>
@@ -74,23 +74,27 @@ export const ProfileHome = () => {
                                                 </Typography>
                                                 <Typography paragraph p={0} m={0} color='text.secondary' fontSize={13}
                                                 >
-                                                    { profile?.posts?.length } Tweets
+                                                    {profile?.posts?.length} Tweets
                                                 </Typography>
                                             </Stack>
                                         </Typography>
                                     </Stack>
                                 </Stack>
                             </Box>
-                            <PortableSearchBox/>
+                            <PortableSearchBox />
                         </Stack>
                         <Stack direction={"row"} spacing={4} sx={{ marginTop: "65px" }}>
                             <Profile {...profile} />
                             <Stack >
                                 <WhotoFollow />
+                                <Privacy />
+                            </Stack>
+                            {/* <Stack >
+                                <WhotoFollow />
                                 <WhotoFollow />
                                 <WhotoFollow />
                                 <Box p={2}><Privacy /></Box>
-                            </Stack>
+                            </Stack> */}
                         </Stack>
                     </Stack>
                 </Stack>
