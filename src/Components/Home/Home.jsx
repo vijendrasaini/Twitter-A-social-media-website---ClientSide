3619166d11f6ca581c016edb6c33bfde88cf6b05
Vijendra <docs.vijendra@gmail.com>
2022-05-06 13:10:44 +0530

    progress line is added.

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,15 +1,16 @@
-import { Box,Stack } from "@mui/material"
+import { Box, LinearProgress, Stack } from "@mui/material"
 import { LeftMenu } from "../LeftMenu/LeftMenu"
 import { PostingDashbord } from "../PostingDashbord/PostingDashbord"
 import { Privacy } from "../Privacy"
 import { WhotoFollow } from "../WhoToFollowBox/WhotoFollow"
-import {PortableSearchBox } from "../SearchBar/PortableSearchBox"
+import { PortableSearchBox } from "../SearchBar/PortableSearchBox"
 import { FeedPostBox } from "../FeedPostBox/FeedPostBox"
 import { HomeHeader } from "../HomeHeader/HomeHeader"
+import { useSelector } from "react-redux"
 
 
 export const Home = () => {
-
+    const { loading } = useSelector(store => store)
 
     return (
         <div
@@ -18,14 +19,17 @@ export const Home = () => {
             <Stack direction={"row"}>
                 <LeftMenu />
                 <Stack>
-                    <Stack direction="row" spacing={4} sx={{position:"fixed", background : "white", zIndex : 5}}>
-                        <HomeHeader/>
-                        <PortableSearchBox/>
+                    <Stack direction="row" spacing={4} sx={{ position: "fixed", background: "white", zIndex: 5 }}>
+                        <HomeHeader />
+                        <PortableSearchBox />
                     </Stack>
-                    <Stack direction={"row"} spacing={4} sx={{marginTop:"65px"}}>
+                    <Stack direction={"row"} spacing={4} sx={{ marginTop: "65px" }}>
                         <Box spacing={0} >
                             <PostingDashbord />
-                            <FeedPostBox/>
+                            {loading && <Box sx={{ width: '100%' }}>
+                                <LinearProgress />
+                            </Box>}
+                            <FeedPostBox />
                         </Box>
                         <Stack >
                             <WhotoFollow />
