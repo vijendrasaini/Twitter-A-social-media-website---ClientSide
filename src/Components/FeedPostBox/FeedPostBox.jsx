ff0f01062152896064e2e463e6795f28259c1ef5
Vijendra <docs.vijendra@gmail.com>
2022-05-06 15:12:44 +0530

    added loading to the feed posts

--- src/Components/FeedPostBox/FeedPostBox.jsx
+++ src/Components/FeedPostBox/FeedPostBox.jsx
@@ -1,25 +1,35 @@
 import { Box } from "@mui/material"
 import { useEffect, useState } from "react"
-import { useSelector } from "react-redux"
+import { useSelector, useDispatch } from "react-redux"
 import { BASE_URL } from "../../UniversalData/univeralData"
 import { Post } from "../Post/Post"
+import { setLoading } from "../../Redux/actionCreators"
+import { Loading } from "../Loading/Loading"
+import axios from 'axios'
 
 
 export const FeedPostBox = () => {
 
     const [interestedPosts, setInterestedPosts] = useState([])
-    const { user : { username}} = useSelector(store => store)
+    const { user : { username}, loading} = useSelector(store => store)
+    const dispatch = useDispatch()
     useEffect(()=>{
         getInterestedPost()
     },[])
 
     async function getInterestedPost(){
         const url = `${BASE_URL}/get-posts/interest/${username}`
-        const response = await fetch(url)
-        const posts = await response.json()
-        setInterestedPosts(posts)
+        dispatch(setLoading({ ...loading, postLoading : true }))
+        try {
+            const response = await axios.get(url)
+            const posts = response.data
+            setInterestedPosts(posts)
+        } catch (error) {
+            console.log({ message : error.message})
+        }
+        dispatch(setLoading({ ...loading, postLoading : false }))
     }
-    return (
+    return loading?.postLoading? <Loading/> : (
         <Box >
             {!interestedPosts.length || !Array.isArray(interestedPosts) ?
                 <Box pt={5}>
