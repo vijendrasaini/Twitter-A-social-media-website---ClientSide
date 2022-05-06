0f16bb242c392206a8a01709da4fd48cd87485bd
Vijendra <docs.vijendra@gmail.com>
2022-05-06 16:02:32 +0530

    url is changed to search users on search bar

--- src/Redux/actionCreators.js
+++ src/Redux/actionCreators.js
@@ -1,3 +1,4 @@
+import { BASE_URL } from '../UniversalData/univeralData'
 import { FOLLOWING_STATUS, ISSEARCHING, LOADING, SETSEARCHRESULT, SETUSER} from './actions'
 
 
@@ -10,8 +11,12 @@ export const setLoading = (payload) => ({ type : LOADING, payload})
 export const performSearch = (searchText)=> async (dispatch)=>{
     if(searchText == "")
         return dispatch(setSearchResult([]))
-    const url = `http://localhost:7000/search?search=${searchText}`
-    const response = await fetch(url)
-    const result = await response.json()
-    dispatch(setSearchResult(result))
+    const url = `${BASE_URL}/search?search=${searchText}`
+    try {
+        const response = await fetch(url)
+        const result = await response.json()
+        dispatch(setSearchResult(result))
+    } catch (error) {
+        console.log({message : error.message})
+    }
 }
